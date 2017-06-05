872cb5849445c3fb7b55f55d2d47d2c9320fa238
Andrea Romanoni <andrearomanoni@gmail.com>
2017-06-05 13:56:40 +0200

    Fixed small issue in configuration options

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-//#include <PhotometricRefinement.h>
-#include <TestMeshSubdivision.h>
+#include <PhotometricRefinement.h>
+//#include <TestMeshSubdivision.h>
 
 int main(int argc, char **argv) {
-  TestMeshSubdivision t;
+  /*TestMeshSubdivision t;
 
-  t.testMeshSubdivision();
+  t.testMeshSubdivision();*/
 
-  /*
+
   if(argc == 2){
     PhotometricRefinement pr(argv[1]);
     pr.run();
   }else{
     std::cout<<"ERROR: Main --> configuration file path is missing"<<std::endl;
-  }*/
+  }
   return 0;
 }
 
