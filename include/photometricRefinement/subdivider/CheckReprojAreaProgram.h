62c24d02bddd355ac3972625ab54484ab2ceadc4
Andrea Romanoni <andrearomanoni@gmail.com>
2017-06-05 16:33:37 +0200

    missing file added

---
+++ include/photometricRefinement/subdivider/CheckReprojAreaProgram.h
@@ -0,0 +1,62 @@
+/*
+ * CheckReprojAreaProgram.h
+ *
+ *  Created on: Jun 5, 2017
+ *      Author: andrea
+ */
+
+#ifndef SRC_SUBDIVIDER_CHECKREPROJAREAPROGRAM_H_
+#define SRC_SUBDIVIDER_CHECKREPROJAREAPROGRAM_H_
+
+#include <ShaderProgram.h>
+#include <glm.hpp>
+class CheckReprojAreaProgram : public ShaderProgram{
+public:
+  CheckReprojAreaProgram(int imageWidth, int imageHeight);
+  virtual ~CheckReprojAreaProgram();
+
+  void compute(bool show =false);
+  void createTransformFeedback(int length);
+  void resetTransformFeedback(int length);
+
+  void setFeedbackLength(GLuint feedbackLength) {
+    feedbackLength_ = feedbackLength;
+  }
+
+  const std::vector<float>& getFeedbackTr() const {
+    return feedbackTr;
+  }
+
+  void setDepthTexture(GLuint depthTexture) {
+    depthTexture_ = depthTexture;
+  }
+
+
+  void setMvp(const glm::mat4& mvp) {
+    this->mvp = mvp;
+  }
+
+  void setArea(const float area) {
+    this->area_ = area;
+  }
+
+private:
+  void init();
+
+  void createAttributes();
+  void createUniforms();
+
+  GLuint posAttrib_;
+  GLuint feedbackBuffer_;
+  GLuint feedbackLength_;
+  std::vector<float> feedbackTr;
+  GLuint depthTexture_;
+  GLuint mvp1CollID_, shadowMapIdColl_,areaId_;
+
+  GLint posAttribCollId_;
+
+  float area_;
+  glm::mat4 mvp;
+};
+
+#endif /* SRC_SUBDIVIDER_CHECKREPROJAREAPROGRAM_H_ */
