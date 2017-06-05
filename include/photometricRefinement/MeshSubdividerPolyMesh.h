e2931f43ddc08259db4e193c60ee89f696e03a0c
Andrea Romanoni <andrearomanoni@gmail.com>
2017-06-05 09:10:55 +0200

    meshsubdivider now on polyhedron

---
+++ include/photometricRefinement/MeshSubdividerPolyMesh.h
@@ -0,0 +1,17 @@
+/*
+ * MeshSubdividerPolyMesh.h
+ *
+ *  Created on: Jun 5, 2017
+ *      Author: andrea
+ */
+
+#ifndef SRC_MESHSUBDIVIDERPOLYMESH_H_
+#define SRC_MESHSUBDIVIDERPOLYMESH_H_
+
+class MeshSubdividerPolyMesh {
+public:
+  MeshSubdividerPolyMesh();
+  virtual ~MeshSubdividerPolyMesh();
+};
+
+#endif /* SRC_MESHSUBDIVIDERPOLYMESH_H_ */
---
+++ src/MeshSubdividerPolyMesh.cpp
@@ -0,0 +1,18 @@
+/*
+ * MeshSubdividerPolyMesh.cpp
+ *
+ *  Created on: Jun 5, 2017
+ *      Author: andrea
+ */
+
+#include "MeshSubdividerPolyMesh.h"
+
+MeshSubdividerPolyMesh::MeshSubdividerPolyMesh() {
+  // TODO Auto-generated constructor stub
+
+}
+
+MeshSubdividerPolyMesh::~MeshSubdividerPolyMesh() {
+  // TODO Auto-generated destructor stub
+}
+
