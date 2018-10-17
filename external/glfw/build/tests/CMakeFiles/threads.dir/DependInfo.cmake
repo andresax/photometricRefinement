3424a1c1b48e78d05f781fb794f59a962bf271e5
Andrea Romanoni <andrearomanoni@gmail.com>
2018-10-17 15:09:52 +0200

    added external

---
+++ external/glfw/build/tests/CMakeFiles/threads.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/andrea/myLib_andSDK/glfw-3.1.2/deps/tinycthread.c" "/home/andrea/myLib_andSDK/glfw-3.1.2/build/tests/CMakeFiles/threads.dir/__/deps/tinycthread.c.o"
+  "/home/andrea/myLib_andSDK/glfw-3.1.2/tests/threads.c" "/home/andrea/myLib_andSDK/glfw-3.1.2/build/tests/CMakeFiles/threads.dir/threads.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/andrea/myLib_andSDK/glfw-3.1.2/build/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
